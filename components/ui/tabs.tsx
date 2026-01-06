c3ddffec6fb67a32be576c55a1547e00592126d7
Jake Torgerson <44759116+wackyvert@users.noreply.github.com>
2026-01-06 13:20:34 -0600

    Update tabs.tsx

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -26,7 +26,7 @@ function TabsList({
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        'bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]',
+        'bg-muted text-muted-foreground inline-flex h-10 w-full items-center rounded-lg p-1 overflow-x-auto',
         className,
       )}
       {...props}
@@ -42,7 +42,15 @@ function TabsTrigger({
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "data-[state=active]:bg-background dark:data-[state=active]:text-foreground \
+         focus-visible:ring-2 focus-visible:ring-ring \
+         text-foreground dark:text-muted-foreground \
+         inline-flex items-center justify-center gap-1.5 \
+         rounded-md border border-transparent px-4 py-2 text-sm font-medium \
+         whitespace-nowrap transition \
+         disabled:pointer-events-none disabled:opacity-50 \
+         data-[state=active]:shadow-sm \
+         [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className,
       )}
       {...props}
